7fcaae03a3d0e9116b9041ac0388d026865c9734
Katar89 <124302382+Katar89@users.noreply.github.com>
2024-05-10 00:58:39 -0500

    RefugeeController.java

--- apirestbd/src/main/java/com/example/refugee/apirestbd/refugio/RefugeeController.java
+++ apirestbd/src/main/java/com/example/refugee/apirestbd/refugio/RefugeeController.java
@@ -1,11 +1,16 @@
 package com.example.refugee.apirestbd.refugio;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import lombok.RequiredArgsConstructor;
+
 
 @RestController
 @RequestMapping("/refugee")
@@ -18,6 +23,24 @@ public class RefugeeController {
     public void createRefugee(@RequestBody Refugee refugee)
     {
          refugeeService.createRefugee(refugee);
+    }
+
+    @PutMapping("/{id}")
+    public void updateRefugee(@PathVariable("id") Integer id, @RequestBody Refugee UpdRefugee)
+    {
+        refugeeService.updateRefugee(id, UpdRefugee);
+    }
+
+    @GetMapping("/{id}")
+    public Refugee searchRefugeeID(@PathVariable Integer id) 
+    {
+        return refugeeService.searchRefugeeID(id);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteRefugeeID(@PathVariable Integer id)
+    {
+        refugeeService.deleteRefugeeID(id);
     }
 
 }
